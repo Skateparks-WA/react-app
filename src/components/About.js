5a63b18ec42510b6ce1a6e5ab256205b1eef292b
Tek <tekthree.gmail.com>
2021-05-04 17:03:28 -0700

    teams page

--- src/components/About.js
+++ src/components/About.js
@@ -1,23 +1,38 @@
 import React, { Component } from "react";
-import { Jumbotron, Button, Col, Image,Row, Container } from "react-bootstrap";
+import { Jumbotron, Button, Col, Image, Row, Container } from "react-bootstrap";
 import Navbar from "./Navbar";
-import tek from '../assets/img/100pxHeadshot.jpg'
+import tek from "../assets/img/tek.png";
 
 export default class About extends Component {
   render() {
     return (
-      <div>
+      <div style={{ backgroundColor: '#e4e4e4', paddingBottom: '20%'}}>
         <Navbar />
-        <Container>
-
-        <div>
+        <Container style={{ marginTop: "160px" }}>
           <Row>
-          <Col xs={6} md={7}>
-            <Image src={tek} roundedCircle />
-          </Col>
-          <div>
-            <Col xs={6} md={7}>
-              <Jumbotron>
+            <Col xs={6} md={4}>
+              <Image src={tek} rounded />
+            </Col>
+            <Col xs={6} md={8}>
+              <Jumbotron style={{backgroundColor: '#ffffff', marginTop: "60px" }}>
+                <h1>Tek Jones</h1>
+                <p>
+                  This is a simple hero unit, a simple jumbotron-style component
+                  for calling extra attention to featured content or
+                  information.
+                </p>
+                <p>
+                  <Button variant="primary">Git Hub</Button>
+                </p>
+              </Jumbotron>
+            </Col>
+          </Row>
+          <Row style={{ marginTop: "90px" }}>
+            <Col xs={6} md={4}>
+              <Image src={tek} rounded />
+            </Col>
+            <Col xs={6} md={8}>
+              <Jumbotron style={{backgroundColor: '#ffffff', marginTop: "60px" }}>
                 <h1>Hello, world!</h1>
                 <p>
                   This is a simple hero unit, a simple jumbotron-style component
@@ -25,14 +40,47 @@ export default class About extends Component {
                   information.
                 </p>
                 <p>
-                  <Button variant="primary">Learn more</Button>
+                  <Button variant="primary">Git Hub</Button>
                 </p>
               </Jumbotron>
             </Col>
-          </div>
-
           </Row>
-        </div>
+          <Row style={{ marginTop: "90px" }}>
+            <Col xs={6} md={4}>
+              <Image src={tek} rounded />
+            </Col>
+            <Col xs={6} md={8}>
+              <Jumbotron style={{backgroundColor: '#ffffff', marginTop: "60px" }}>
+                <h1>Hello, world!</h1>
+                <p>
+                  This is a simple hero unit, a simple jumbotron-style component
+                  for calling extra attention to featured content or
+                  information.
+                </p>
+                <p>
+                  <Button variant="primary">Git Hub</Button>
+                </p>
+              </Jumbotron>
+            </Col>
+          </Row>
+          <Row style={{ marginTop: "90px",}}>
+            <Col xs={6} md={4}>
+              <Image src={tek} rounded />
+            </Col>
+            <Col xs={6} md={8}>
+              <Jumbotron style={{backgroundColor: '#ffffff', marginTop: "60px" }}>
+                <h1>Hello, world!</h1>
+                <p>
+                  This is a simple hero unit, a simple jumbotron-style component
+                  for calling extra attention to featured content or
+                  information.
+                </p>
+                <p>
+                  <Button variant="primary">Git Hub</Button>
+                </p>
+              </Jumbotron>
+            </Col>
+          </Row>
         </Container>
       </div>
     );
